5249f0866e7c4f1774b6e927f4f61aea9f275c5f
Romain QUECHON <romain.quechon@gmail.com>
2020-11-11 17:01:03 +0100

    Exercice 2 completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+CATEGORIES = ['chinese', 'italian', 'japanese', 'french', 'belgian']
+
+puts "Clearing database"
+Restaurant.destroy_all
+
+puts 'Creating 5 fake restaurants...'
+
+5.times do
+  restaurant = Restaurant.new(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    category: CATEGORIES.sample
+  )
+  restaurant.save!
+end
+puts 'Finished!'
